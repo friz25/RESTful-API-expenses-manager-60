f421dfcbae42ce9654175f8b9dfc50a68003a0cd
frizmob@gmail.com <friz48625>
2022-06-23 13:17:26 +0300

    v2

--- expenses_manager.py
+++ expenses_manager.py
@@ -1,6 +1,22 @@
 from flask import Flask, request, jsonify
 app = Flask( __name__ )
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    data = []
+    if request.method == 'POST':
+        data = [dict(id='1', name='max', email='max@gmail.com')]
+        response = jsonify(data) # Converts your data strcuture into JSON format
+        response.status_code = 202 #202 == "Accepted"
+
+        return response
+    else:
+        data = [dict(id='none', name='none', email='none')]# Data structure of JSON format
+        response = jsonify(data)
+        response.status_code = 406 # 406 == "Not Acceptable"
+
+        return response
+
 @app.route('/')
 def index():
     return 'Index page'
